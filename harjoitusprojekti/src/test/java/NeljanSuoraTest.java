af4bd2dcec0cfe478d5821f09e531f13bcaca4f6
pekkap <pekka.prepula@gmail.com>
2019-04-02 23:31:16 +0300

    lisää testejä

--- harjoitusprojekti/src/test/java/NeljanSuoraTest.java
+++ harjoitusprojekti/src/test/java/NeljanSuoraTest.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+import java.util.HashMap;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -12,6 +13,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import NeljanSuora.logic.Usercontrol;
 import NeljanSuora.domain.User;
+import NeljanSuora.logic.Usercontrol;
 
 /**
  *
@@ -20,27 +22,70 @@ import NeljanSuora.domain.User;
 public class NeljanSuoraTest {
     
     User defaultUser;
+    Usercontrol control;
     
     @Before
     public void setUp() {
         defaultUser = new User("Petteri");
+        control = new Usercontrol(new HashMap<>());
     }
     
     
     @Test
-    public void getNameIsWorkingProperly() {
+    public void getNameinUserIsWorkingProperly() {
         assertEquals("Petteri", defaultUser.getName());
     }
 
     @Test
-    public void toStringIsWorkingProperly() {
+    public void toStringinUserIsWorkingProperly() {
         assertEquals("Petteri", defaultUser.toString());
     }
     
     @Test
-    public void getNameModifiedIsWorkingProperly() {
+    public void getNameModifiedinUserIsWorkingProperly() {
         User testUser = new User("tapIO   ");
         
         assertEquals("TAPIO", testUser.getNameModified());
+    }
+    
+    @Test
+    public void addingUserReturnsTrueWhenNameIsNotTaken() {
+        boolean added = control.addUser("Teppo");
+        
+        assertEquals(true, added);
+    }
+    
+    @Test
+    public void addingUserReturnsFalseWhenNameIsTaken() {
+        control.addUser("Teppo");
+        boolean added = control.addUser("Teppo");
+        
+        assertEquals(false, added);
+    }
+    
+    @Test
+    public void addUserReturnsTrueWhenUsingUserParameterAndUserHasNotBeenAdded() {
+        boolean added = control.addUser(defaultUser);
+        
+        assertEquals(true, added);
+    }
+    
+    @Test
+    public void addUserReturnsFalseWhenUsingUserParameterAndUserHasBeenAdded() {
+        control.addUser(defaultUser);
+        boolean added = control.addUser(defaultUser);
+        
+        assertEquals(false, added);
+    }
+    
+    @Test
+    public void userExistsReturnsFalseWhenUserHasNotBeenAdded() {
+        assertEquals(false, control.userExists("test"));
+    }
+    
+    @Test
+    public void userExistsReturnsTrueWhenUserHasBeenAdded() {
+        control.addUser("Teppo");
+        assertEquals(true, control.userExists("Teppo"));
     }
 }
